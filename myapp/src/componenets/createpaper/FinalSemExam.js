89b4889747011e2c37e770c90d2cc35b5bada716
10shekhawat <138010159+10shekhawat@users.noreply.github.com>
2023-11-03 18:55:56 +0530

    some

--- myapp/src/componenets/createpaper/FinalSemExam.js
+++ myapp/src/componenets/createpaper/FinalSemExam.js
@@ -1,18 +1,33 @@
 import React from 'react'
 
 const FinalSemExam = () => {
-  return (
-    <>
-    <div className='container'>
-        <div className='row'>
-            <div className='col-12'>
-                
-            </div>
-        </div>
-    </div>
+    let name = "Chemistry"
+    return (
+        <>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-12'>
+                        <div>
+                            <h1 className='head'>Preview</h1>
+                            <p className='nop'>Name of paper</p>
+                            <h1 className='head' style={{ fontWeight: "600" }}>{name} Final Sem Of Examination </h1>
+                        </div>
 
-    </>
-  )
+
+                    </div>
+                    <div style={{ height: "40vh", borderTop: "1px solid #565656", marginTop: "10px" }}>
+                   <div className='row'>
+                        <div className='col-12'>
+                            <h1 className='tc'>Topic & Composition</h1>
+                            
+                    </div>
+                   </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
 }
 
 export default FinalSemExam
